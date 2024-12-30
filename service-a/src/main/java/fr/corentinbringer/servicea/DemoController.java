4a26c0b0a0529116e837af42c29259a98d9a6bdc
Corentin Bringer <corentinbringer@ik.me>
2024-12-30 22:25:01 +0100

    service a

--- service-a/src/main/java/fr/corentinbringer/servicea/DemoController.java
+++ service-a/src/main/java/fr/corentinbringer/servicea/DemoController.java
@@ -1,2 +1,17 @@
-package fr.corentinbringer.servicea;public class DemoController {
+package fr.corentinbringer.servicea;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @Value("${app.env:n/a}")
+    private String env;
+
+    @GetMapping("/demo")
+    public String demo() {
+        return env;
+    }
 }
